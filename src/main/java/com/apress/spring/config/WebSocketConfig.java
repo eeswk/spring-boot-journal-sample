2d8a16cf3be81d5abd6d1c1645946b9ef5f146c3
themorning <themorning84@gmail.com>
2020-03-03 02:34:56 +0900

    websocket 기능 추가

--- src/main/java/com/apress/spring/config/WebSocketConfig.java
+++ src/main/java/com/apress/spring/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.apress.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
